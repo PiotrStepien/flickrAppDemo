cdebe24fc1a50195e9f0c1b6f133a71fb3d53881
Piotr Stepien <piotr.stepien@altimi.com>
2017-02-15 14:33:32 +0100

    new alert added

--- AlertService.swift
+++ AlertService.swift
@@ -23,4 +23,17 @@ class AlertService {
         return alertController
     }
     
+    func fourOptionsAllert(title: String, message: String?, firstButtonTitle: String, secondButtonTitle: String, thirdButtonTitle: String, fourthButtonTitle: String, firstCompletionHandler: ((UIAlertAction) -> ())?, secondCompletionHandler: ((UIAlertAction) -> ())?, thirdCompletionHandler: ((UIAlertAction) -> ())?, fourthCompletionHandler: ((UIAlertAction) -> ())? ) -> UIAlertController {
+        let firstAction = UIAlertAction(title: firstButtonTitle, style: .default, handler: firstCompletionHandler)
+        let secondAction = UIAlertAction(title: secondButtonTitle, style: .default, handler: secondCompletionHandler)
+        let thirdAction = UIAlertAction(title: thirdButtonTitle, style: .default, handler: thirdCompletionHandler)
+        let fourthAction = UIAlertAction(title: fourthButtonTitle, style: .cancel, handler: fourthCompletionHandler)
+        alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(firstAction)
+        alertController.addAction(secondAction)
+        alertController.addAction(thirdAction)
+        alertController.addAction(fourthAction)
+        return alertController
+    }
+    
 }
