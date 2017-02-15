781769a521d71d9064089e4a4305061ec51316e5
Piotr Stepien <piotr.stepien@altimi.com>
2017-02-15 08:00:12 +0100

    cell init

---
+++ FlickrPhotoCell.swift
@@ -0,0 +1,18 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrDemoApp
+//
+//  Created by Piotr Stępień on 15/02/2017.
+//  Copyright © 2017 Piotr Stepien. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
